038a77ce02a5a1e66cc7cab6e13ea9bba3e8caaa
destructivebasket <sophie.elissa.choi@gmail.com>
2026-09-19 23:44:22 -0700

    feat: added docker

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+FROM node:18-alpine
+
+WORKDIR /app/
+COPY package*.json .
+
+RUN npm ci
+COPY index.js .
+
+EXPOSE 5001
+CMD ["node", "index.js"]
--- index.js
+++ index.js
@@ -1,39 +1,37 @@
-const express = require('express')
-const cowsay = require('cowsay')
-const cors = require('cors')
+const express = require("express");
+const cowsay = require("cowsay");
+const cors = require("cors");
 
-const app = express()
+const app = express();
 
-app.get('/api/cow/:say', cors(), async (req, res, next) => {
-    try {
-        const text = req.params.say
-        const moo = cowsay.say({ text })
-        res.json({moo})
-    }
-    catch(err) {
-        next(err)
-    }
-})
+app.get("/api/cow/:say", cors(), async (req, res, next) => {
+	try {
+		const text = req.params.say;
+		const moo = cowsay.say({ text });
+		res.json({ moo });
+	} catch (err) {
+		next(err);
+	}
+});
 
-app.get('/api/cow/', cors(), async(req, res, next) => {
-    try {
-        const moo = cowsay.say({ text: 'Hello World'})
-        res.json({ moo })
-    }
-    catch (err) {
-        next(err)
-    }
-})
+app.get("/api/cow/", cors(), async (req, res, next) => {
+	try {
+		const moo = cowsay.say({ text: "Hello World" });
+		res.json({ moo });
+	} catch (err) {
+		next(err);
+	}
+});
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
-    console.log(`Mixing it up on port ${PORT}`)
-})
+	console.log(`Mixing it up on port ${PORT}`);
+});
 
-const path = require('path')
+const path = require("path");
 
-app.use(express.static(path.join(__dirname, 'client/build')))
+app.use(express.static(path.join(__dirname, "client/build")));
 // Anything that doesn't match the above, send back index.html
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/client/build/index.html'))
-})
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname + "/client/build/index.html"));
+});
